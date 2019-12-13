9157de2c730d40cee8f18bc1f8b89ed9c013c62d
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2019-12-12 23:40:30 -0500

    Added thread safety with fixed sized queue

--- include/process.hpp
+++ include/process.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#include <array>
+#include <condition_variable>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <vector>
@@ -109,20 +112,110 @@ namespace Harpoon
         return processes;
     }
 
-    template <typename Input, typename Output>
-    class Buffer
-    {};
-
-    template <typename Output>
-    class Buffer<void, Output>
+    template <typename Type, std::size_t Size_>
+    class Queue
     {
+        std::array<Type, Size_> data;
+        using Iterator = typename std::array<Type, Size_>::iterator;
+        Iterator first;
+        Iterator last;
+
+        std::mutex push_mutex;
+        std::condition_variable push_cv;
+
+        std::mutex pop_mutex;
+        std::condition_variable pop_cv;
     public:
-        std::vector<Output> data;
+        Queue() : data{}, first{ data.begin() }, last{ data.begin() }
+        {}
+
+        template <typename T>
+        void Push(T&& t)
+        {
+            std::unique_lock lock{ push_mutex };
+            push_cv.wait(lock, [this](){ return Size() < Size_; });
+
+            if (last == data.end())
+                last = data.begin();
+            *last++ = std::forward<T>(t);
+
+            lock.unlock();
+            if (Size() == 1)
+                pop_cv.notify_one();
+            else
+                pop_cv.notify_all();
+        }
+
+        Type& Pop()
+        {
+            std::unique_lock lock{ pop_mutex };
+            pop_cv.wait(lock, [this](){ return Size() > 0; });
+
+            if (first == data.end())
+                first = data.begin();
+            auto& elem = *first++;
+
+            lock.unlock();
+            if (Size() == 1)
+                push_cv.notify_one();
+            else
+                push_cv.notify_all();
+            
+            return elem;
+        }
+
+        std::size_t Size()
+        {
+            std::size_t dist = std::distance(first, last);
+            return first <= last ? dist : Size_ + dist;
+        }
     };
 
-    template <typename Input>
-    class Buffer<Input, void>
-    {};
+    template <typename Input, typename Output, std::size_t Size>
+    class Buffer
+    {
+        Queue<Input, Size>& input;
+        Queue<Output, Size>& output;
+    public:
+        Buffer(Queue<Input, Size>& input, Queue<Output, Size>& output) : input{ input }, output{ output }
+        {}
+
+        template <typename Functor>
+        void operator()(Functor&& f)
+        {
+            output.Push(f(input.Pop()));
+        }
+    };
+
+    template <typename Output, std::size_t Size>
+    class Buffer<void, Output, Size>
+    {
+        Queue<Output, Size>& output;
+    public:
+        Buffer(Queue<Output, Size>& output) : output{ output }
+        {}
+
+        template <typename Functor>
+        void operator()(Functor&& f)
+        {
+            output.Push(f());
+        }
+    };
+
+    template <typename Input, std::size_t Size>
+    class Buffer<Input, void, Size>
+    {
+        Queue<Input, Size>& input;
+    public:
+        Buffer(Queue<Input, Size>& input) : input{ input }
+        {}
+
+        template <typename Functor>
+        void operator()(Functor&& f)
+        {
+            f(input.Pop());
+        }
+    };
 
 	template <typename Type>
 	class Process : public Base_Process
@@ -145,31 +238,39 @@ namespace Harpoon
 			hook();
 		}
 
+        bool Active() const 
+        {
+            return true;
+        }
+
         int Execute(std::vector<std::string> const& args) final
         {
             using Main_Function = decltype(&Type::Main);
-            using Input_Type = Meta::Unwrap<Meta::Args<Main_Function>>;
-            using Ouput_Type = Meta::Return_Type<Main_Function>;
+            using Input_Type = std::decay_t<Meta::Unwrap<Meta::Args<Main_Function>>>;
+            using Ouput_Type = std::decay_t<Meta::Return_Type<Main_Function>>;
 
-            Buffer<Input_Type, Ouput_Type> buffer { };
+            Queue<std::string, 16> i;
+            i.Push("1");
+            i.Push("2");
+            i.Push("3");
+            i.Push("4");
+            i.Push("5");
+            Queue<int, 16> o;
+
+            Buffer<Input_Type, Ouput_Type, 16> buffer { i, o };
 
             threads.push_back(std::thread{ [&]()
             {
-                int i = 10;
-                while (true)
-                {
-                    buffer.data.push_back(Meta::Cast<Type>(*this).Main());
-                    if (--i == 0)
-                        return;
-                }
+                while (Meta::Cast<Type>(*this).Active())
+                    buffer([&](auto& ... x){ return Meta::Cast<Type>(*this).Main(x...); });
             } });
 
             for(auto& t : threads)
                 if (t.joinable())
                     t.join();
 
-            for (auto& x : buffer.data)
-                std::cout << x << std::endl;
+            for (int i = 0; i < 5; i++)
+                std::cout << o.Pop() << std::endl;
 
             return 0;
         }
--- test/test.cpp
+++ test/test.cpp
@@ -2,13 +2,28 @@
 
 class Test : public Harpoon::Process<Test>
 {
-    int i{0};
+    int i{ 0 };
 public:
     Test() : Process{ "Test" } {}
 
-    int Main()
+    // int Main()
+    // {
+    //     return i++ * 2;
+    // }
+
+    // void Main(std::string const& s)
+    // {
+    //     std::cout << std::stoi(s) * 2 << std::endl;
+    // }
+
+    int Main(std::string const& s)
     {
-        return i++;
+        return std::stoi(s) * 2;
+    }
+
+    bool Active()
+    {
+        return i++ < 5;
     }
 };
 

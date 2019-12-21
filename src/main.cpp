99f1cc085e1e7d87d72b0ac5495e32ef784f60c4
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2019-12-21 17:03:51 -0500

    fixed queue bugs

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,15 @@ int main(int argc, char** argv)
 {
     std::vector<std::string> args;
     std::copy(argv, argv + argc, std::back_inserter(args));
+    
+    std::vector<std::thread> threads;
 
     for (auto& p : Processes())
-        p->Execute(args);
-    
+        threads.emplace_back(p->Execute(args));
+
+    for (auto& thread : threads)
+        if (thread.joinable())
+            thread.join();
+
     return 0;//to fix return something based on the processes;
 }
